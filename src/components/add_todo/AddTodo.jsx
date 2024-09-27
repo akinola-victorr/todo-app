f4479f1e71e34abb94d4a6bc8f378512ac5656be
victor Akinola <akinolavictor226@gmail.com>
2024-09-27 11:36:15 +0100

    Completed Add_todo

---
+++ src/components/add_todo/AddTodo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./AddTodo.scss";
+export default function AddTodo() {
+  return (
+    <div className="addTodo">
+      <input className="addTodo__input" type="text" placeholder="Add a todo" />
+      <button className="addTodo__btn">+</button>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Container from "../components/container/Container";
 import Header from "../components/header/Header";
-
+import AddTodo from "../components/add_todo/AddTodo";
 export default function Home() {
   return (
     <Container>
       <Header />
+      <AddTodo />
     </Container>
   );
 }
